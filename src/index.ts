dc5823d96b1f3239370dd3a87eae87cff960c51e
Giovanni Estevam <giovannibestevam@gmail.com>
2024-09-03 17:12:45 -0300

    Update: adjust environment variable

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,12 @@ const agent = new AtpAgent({
     service: 'https://bsky.social',
 })
 
-const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-);
-
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+    })
 });
   
 const db = getFirestore();
